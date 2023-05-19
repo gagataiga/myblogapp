b64eb78c3d034c1418b62f74979d6cf8afa70abf
TaigaShiga <gaitatg97@gmail.com>
2023-05-20 00:59:29 +0200

    feat: add models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    id = models.CharField(primary_key=True, max_length=255, unique=True)
+    username = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.username
+      
+class Post(models.Model):
+    id = models.CharField(primary_key=True, max_length=255, unique=True)
+    userId = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+  
+    def __str__(self):
+        return self.title
